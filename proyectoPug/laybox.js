9e378625985790c5b2a721c4fbdabd3626d71a92
FelipeMILTON <gonza-lo-les@hotmail.com>
2021-05-04 13:33:24 -0400

    commint

---
+++ proyectoPug/laybox.js
@@ -0,0 +1,5 @@
+const imagenes = document.querySelector('.contenedor-galeria')
+const imagenesLigth = document.querySelector('.agregar-imagen')
+const contenedorLigth = document.querySelector('.imagen-ligth')
+
+
